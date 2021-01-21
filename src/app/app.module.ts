b878fa545e5926214b2be8061f11975677f7ac81
GeoshanAllen <geoshanallen@gmail.com>
2021-01-21 16:55:08 +0530

    hi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatMenuModule} from '@angular/material/menu';
 import { MatIconModule} from '@angular/material/icon';
 import { MatButtonModule} from '@angular/material/button';
@@ -28,7 +30,8 @@ import {MatButtonToggleModule} from "@angular/material/button-toggle";
     NgbModule,
     MatSlideToggleModule,
     MatTabsModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,BrowserAnimationsModule
+
 
   ],
   exports:[
@@ -39,10 +42,13 @@ import {MatButtonToggleModule} from "@angular/material/button-toggle";
     MatButtonModule,
     MatSlideToggleModule,
     MatTabsModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,BrowserAnimationsModule
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
